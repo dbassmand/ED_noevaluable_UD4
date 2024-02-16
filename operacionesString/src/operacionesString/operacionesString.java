95fb628c67c9be4d3ab05f4f00b69f3f7abd6ca0
dbassmand <bassman.dani@gmail.com>
2024-02-16 20:28:02 +0100

    Version v0.2

--- operacionesString/src/operacionesString/operacionesString.java
+++ operacionesString/src/operacionesString/operacionesString.java
@@ -4,18 +4,19 @@ public class operacionesString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int cantidadDeCaracteres = "Jose Daniel Romero Perez".length();
+		int cantidadDeCaracteres = "Juan Galvez Ortega".length();
 		System.out.println("La cantidad de caracteres es " + cantidadDeCaracteres);
 		
 		//Extraer una porción de texto
 		
 		String lF ="El primer lenguaje que voy a aprender es JAVA";
-		String extraccion = lF.substring(41,45);
-		System.out.println("LA extraccion es " + extraccion);
+		String extraccion = lF.substring(14,25);
+		System.out.println("Las letras extraidas son" + extraccion);
+		System.out.println("Estoy cursando mis estudios en Cesur");
 		
 		//Determinar si dos cadenas son iguales
 		String saludo1 = "Hola mundo";
-		String saludo2 = "Hola Mundo";
+		String saludo2 = "Adios Mundo";
 		boolean esIgual = saludo1.equals(saludo2);
 		System.out.println("Son iguales la cadenas? " + esIgual);
 	
@@ -26,11 +27,11 @@ public class operacionesString {
 				System.out.println("Son iguales la cadenas? " + esIgualIgnoraMayusculas);
 	    //devolver el indice de una letra
 				
-				int indice = "Argentina".indexOf("e");
-				System.out.println("Le letra 'e' esta en la posicion " + indice);
+				int indice = "Londres".indexOf("o");
+				System.out.println("Le letra 'o' esta en la posicion " + indice);
 		//Extraer letra
 				
-				char letraExtraida = "Argentina".charAt(3);
+				char letraExtraida = "Africa".charAt(3);
 				System.out.println(letraExtraida);
 				
 		//Determinar si una palabra está en una frase;
